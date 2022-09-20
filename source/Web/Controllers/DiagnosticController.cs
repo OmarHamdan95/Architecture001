2e89031e4e7458f16b50c4bf0f6cf6427577de6a
OmarHamdan95 <81062398+OmarHamdan95@users.noreply.github.com>
2022-09-20 22:13:41 +0300

    [Arch] handel Query Expression

--- source/Web/Controllers/DiagnosticController.cs
+++ source/Web/Controllers/DiagnosticController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using DotNetCore.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using System.Reflection;
 using Architecture.Database.Queries;
 using Architecture.Database.Queries.QueriesCustome;
 using Architecture.Domain;
+using Architecture.Model;
 using Architecture.Model.Tree;
 using DotNetCore.AspNetCore;
 
@@ -33,4 +35,27 @@ public sealed class DiagnosticController : ControllerBase
 
     [HttpGet("treeFalate")]
     public async Task<IActionResult> flate(long? id) =>  (await _getTreeBYIdQuery.QueryAsync(id.Value)).ApiResult();
+
+    [HttpGet("Test")]
+    public async Task<IActionResult> test(long? id)
+    {
+        Expression <Func<Tree, TreeDto>> fun ;
+        fun = x => new TreeDto()
+        {
+            Code = x.Code,
+            Description = new NameDto(x.Description.NameAr, x.Description.NameEn),
+            Id = x.Id,
+            ParentId = x.ParentId,
+            FlateParent = x.FlateParent.Select(f => new TreeDto
+            {
+                Description = new NameDto(f.Parent.Description.NameAr, f.Parent.Description.NameEn),
+                Id = f.ParentId,
+                ParentId = f.GrandParentId,
+            }).ToList()
+
+        };
+        //(long ID, Expression<Func<Tree, TreeDto>> func)  = (id.Value, fun);
+
+        return (await _getTreeBYIdQuery.QueryAsync((id.Value, fun))).ApiResult();
+    }
 }
